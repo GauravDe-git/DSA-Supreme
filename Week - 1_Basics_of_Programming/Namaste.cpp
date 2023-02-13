100d9c78ac8a7c3727d57f553bbac1fabdef52df
GauravDe-git <gm.gauravde@gmail.com>
2023-02-13 20:12:24 +0530

    Feat: Added week 1 class code

--- Week - 1_Basics_of_Programming/Namaste.cpp
+++ Week - 1_Basics_of_Programming/Namaste.cpp
@@ -3,4 +3,14 @@ using namespace std;
 
 int main(){
     cout<<"Namaste Bharat"<<endl;
+
+    cout<<"Enter your favourite number: "<<endl;
+
+    int favNum;
+
+    cin>>favNum;
+
+    cout<<"Your fav num is: "<<favNum<<endl;
+
+    system("pause");
 }
---
+++ Week - 1_Basics_of_Programming/solidRectangle.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+//Program for Solid Rectangle- 4 rows, 5 columns
+int main()
+{
+    for(int row = 0; row < 4; row++)
+    {
+        for(int col =0; col< 5; col++)
+        {
+            cout<<"* ";
+        }
+        cout<<endl;
+    }
+    system("pause");
+}
